963583ae230effd294b5f7035308b084e4842a8f
jakubowiczish <jakub77plotnikowski@gmail.com>
2019-08-22 20:24:37 +0200

    Add README files and solution to RomanToInteger problem

---
+++ src/README.md
@@ -0,0 +1,5 @@
+Problems solved:
+1. https://leetcode.com/problems/two-sum/
+13. https://leetcode.com/problems/roman-to-integer/
+53. https://leetcode.com/problems/maximum-subarray/
+
---
+++ src/RomanToInteger/RomanToInteger.java
@@ -0,0 +1,57 @@
+package RomanToInteger;
+
+public class RomanToInteger {
+    public int romanToInt(String s) {
+        int i, result = 0;
+
+        for (i = 0; i < s.length(); ++i) {
+            switch (s.charAt(i)) {
+                case 'M': {
+                    if (i > 0 && s.charAt(i - 1) == 'C')
+                        result += 800;
+                    else result += 1000;
+                    break;
+                }
+                case 'D': {
+                    if (i > 0 && s.charAt(i - 1) == 'C')
+                        result += 300;
+                    else result += 500;
+                    break;
+                }
+                case 'C': {
+                    if (i > 0 && s.charAt(i - 1) == 'X')
+                        result += 80;
+                    else result += 100;
+                    break;
+                }
+                case 'L': {
+                    if (i > 0 && s.charAt(i - 1) == 'X')
+                        result += 30;
+                    else result += 50;
+                    break;
+                }
+                case 'X': {
+                    if (i > 0 && s.charAt(i - 1) == 'I')
+                        result += 8;
+                    else result += 10;
+                    break;
+                }
+                case 'V': {
+                    if (i > 0 && s.charAt(i - 1) == 'I')
+                        result += 3;
+                    else result += 5;
+                    break;
+                }
+                case 'I': {
+                    result += 1;
+                    break;
+                }
+
+                default:
+                    return 0;
+            }
+        }
+        return result;
+    }
+
+}
